a5ff813fa43cdcbdc5f26fe3477b92da70fc4fb6
EvenChuan <1090800395@qq.com>
2017-08-09 17:01:46 +0800

    图片的绘制+上传

--- js/common.js
+++ js/common.js
@@ -1,7 +1,7 @@
-$(function(){
-  $("header").load("header.html",function(){
-      $("footer").load("footer.html",function(){
-      });
-  });
-
+$(function () {
+    $("header").load("header.html", function () {
+        $("footer").load("footer.html", function () {
+        });
+    });
+    (!$(".ico").length) && $("head").append('<link class="ico" rel="icon" href="images/jicon.ico"/>')
 });
